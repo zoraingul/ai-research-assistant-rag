b0b2dd5e77b7cc269c0d7330c795f7b954edf2bf
zoraingul <zorain.stu@gmail.com>
2026-06-08 14:55:13 +0500

    Implement embeddings and FAISS-based vector store

--- vector_store.py
+++ vector_store.py
@@ -12,3 +12,25 @@ def create_embeddings(chunks, model):
     embeddings = np.array(embeddings).astype("float32")
     return embeddings
 
+def build_faiss_index(embddings):
+    dimensions = embddings.shape[1]
+    index = faiss.IndexFlatL2(dimensions)
+    index.add(embddings)
+    return index
+
+def retrieve_chunks(question , model , index , chunks , k=3):
+    question_embedding = model.encode(question)
+    question_embedding = np.array(question_embedding).astype("float32")
+    question_embedding = question_embedding.reshape(1 , -1)
+    
+    distances , indeces = index.search (
+        question_embedding , 
+        k
+    )
+    
+    retrieved_chunks = []
+    
+    for idx in indeces[0]:
+        retrieved_chunks.append(chunks[idx])
+    
+    return retrieved_chunks
